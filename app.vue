aee3c6961b62dd9d4c81178ddf0cd8c5552c355a
Ariane <ariane@globalfy.com>
2025-02-19 13:16:34 -0300

    feat: create header sidebar layout components and contacts page route

--- app.vue
+++ app.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-import { Button } from "@/components/ui/button";
+import { ref } from "vue";
+import Header from "./components/Header.vue";
+import Sidebar from "./components/Header.vue";
+import Contacts from "./pages/contacts/index.vue";
+import { NuxtLayout } from "#components";
+
+const showSidebar = ref(true);
+
+const toggleSidebar = () => {
+  showSidebar.value = !showSidebar.value;
+};
 </script>
 
 <template>
-  <div class="flex items-center justify-center">
-    <h2 class="flex bg-red-300 p-6 h-10 w-[200px] items-center justify-center">
-      test
-    </h2>
-    <Button>Click me</Button>
-    <div>
-      <h3>test</h3>
-      <h1>test</h1>
-    </div>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
