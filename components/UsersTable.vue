afb33b73cc0538e0eecd370ee608264e6e1265d3
Ariane <ariane@globalfy.com>
2025-02-20 15:17:25 -0300

    feat: added the users table

---
+++ components/UsersTable.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+</script>
+
+<template>
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead>Nome</TableHead>
+        <TableHead class="text-center"> Data de nascimento </TableHead>
+        <TableHead class="text-center">CPF</TableHead>
+
+        <TableHead class="text-center"> Email </TableHead>
+        <TableHead class="text-center"> Telefone </TableHead>
+        <TableHead class="text-center"> Username </TableHead>
+        <TableHead class="text-center"> </TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableCell>Bolinha</TableCell>
+        <TableCell class="text-center"> 02/01/2020 </TableCell>
+        <TableCell class="text-center"> 16578978709 </TableCell>
+        <TableCell class="text-center"> email@email.com </TableCell>
+        <TableCell class="text-center"> 3322 8716 </TableCell>
+        <TableCell class="text-center"> bolinhaUser </TableCell>
+
+        <TableCell class="text-center">
+          <i class="pi pi-pencil cursor-pointer" style="font-size: 1rem"></i>
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
--- pages/usuarios/index.vue
+++ pages/usuarios/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>usuarios</div>
+  <div
+    class="px-6 py-4 flex items-center w-full h-full relative z-0 bg-stone-200 flex-col rounded-2xl gap-10"
+  >
+    <div class="flex justify-between w-full items-center">
+      <h1 class="text-stone-700 text-2xl">Usuários</h1>
+
+      <Button class="cursor-pointer bg-sky-200 text-stone-700 hover:bg-sky-300">
+        Criar novo usuário
+      </Button>
+    </div>
+
+    <UsersTable />
+  </div>
 </template>
