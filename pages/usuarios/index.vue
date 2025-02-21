39cf1034d90c6e97c382180163fa8f70ed18c5ca
Ariane <ariane@globalfy.com>
2025-02-21 11:57:26 -0300

    feat: added create and edit users forms and pages

--- pages/usuarios/index.vue
+++ pages/usuarios/index.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goToEditPage = () => {
+  router.push("/usuarios/criar-usuario");
+};
+</script>
+
 <template>
   <div
     class="px-6 py-4 flex items-center w-full h-full relative z-0 bg-stone-200 flex-col rounded-2xl gap-10"
@@ -5,7 +15,10 @@
     <div class="flex justify-between w-full items-center">
       <h1 class="text-stone-700 text-2xl">Usuários</h1>
 
-      <Button class="cursor-pointer bg-sky-200 text-stone-700 hover:bg-sky-300">
+      <Button
+        @click="goToEditPage"
+        class="cursor-pointer bg-sky-200 text-stone-700 hover:bg-sky-300"
+      >
         Criar novo usuário
       </Button>
     </div>
